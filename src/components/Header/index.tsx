1140dfeacba38f727041dc67b5df78ddeaa64403
Iury Gama <Iuryempregos@gmail.com>
2023-11-20 19:05:02 -0300

    create the Header and import in index

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ContainerHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    width: 100dvw;
+    height: 8dvh;
+    background-color: slategray;
+`
+
+export default function Header () {
+    return(
+        <ContainerHeader>
+
+        </ContainerHeader>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import Head from 'next/head'
 // import Image from 'next/image'
 
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Pagina Principal</h1>
+        <Header />
       </main>
     </>
   )
